92a2e69b9bab51f8a671980431be00bd5c974221
huyhoang17 <phan.huy.hoang@framgia.com>
2018-10-29 16:01:56 +0700

    add IncrementalPCA model

--- src/embedding.py
+++ src/embedding.py
@@ -3,10 +3,10 @@ import random
 
 from keras.datasets import mnist
 from keras.models import model_from_json, Model
-from sklearn.decomposition import PCA
+from sklearn.decomposition import PCA, IncrementalPCA
 
 
-def main():
+def main(mode='pca'):
     (X_train, y_train), (_, _) = mnist.load_data()
     X_train = X_train.reshape(X_train.shape[0], 28, 28, 1)
     X_train = X_train.astype('float32')
@@ -31,12 +31,21 @@ def main():
     new_model.set_weights(model.get_weights())
 
     embs_4096 = new_model.predict(X_train)
-    pca = PCA(n_components=128)
-    embs_128 = pca.fit_transform(embs_4096)
-    with open('../models/embs_128D.pkl', 'wb') as f:
-        pickle.dump(embs_128, f)
-
-    embs_128.tofile('../oss_data/MNIST_tensor.bytes')
+    if mode == 'pca':
+        pca = PCA(n_components=128)
+        embs_128 = pca.fit_transform(embs_4096)
+        with open('../models/embs_128D.pkl', 'wb') as f:
+            pickle.dump(embs_128, f)
+        embs_128.tofile('../oss_data/MNIST_tensor.bytes')
+    elif mode == 'ipca':
+        # http://scikit-learn.org/stable/modules/generated/sklearn.decomposition.IncrementalPCA.html#sklearn.decomposition.IncrementalPCA  # noqa
+        ipca = IncrementalPCA(n_components=128, batch_size=200)
+        embs_128 = ipca.fit_transform(embs_4096)
+        with open('../models/embs_128D_2.pkl', 'wb') as f:
+            pickle.dump(embs_128, f)
+        embs_128.tofile('../oss_data/MNIST_tensor_2.bytes')
+    else:
+        raise NotImplementedError("Mode must be set")
 
 
 if __name__ == '__main__':
